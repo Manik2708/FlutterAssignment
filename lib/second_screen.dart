48716fa994a1103284d89b7837c9658861eba98b
Manik Mehta <mehtamanik96@gmail.com>
2023-12-26 16:01:53 +0530

    Sixth

--- lib/second_screen.dart
+++ lib/second_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutterassignment/check_box.dart';
 import 'package:flutterassignment/constants.dart';
-import 'package:flutterassignment/final_screen.dart';
+import 'package:flutterassignment/third_screen.dart';
 class SecondScreen extends StatefulWidget {
   const SecondScreen({super.key});
 
@@ -12,8 +12,6 @@ class SecondScreen extends StatefulWidget {
 class _SecondScreenState extends State<SecondScreen> {
   List<String> currentState=Constants.fruitsMap.keys.toList();
   String appBarTitle='Fruits';
-  bool checkBoxBool=false;
-  bool onVegetablesScreen=false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,28 +22,20 @@ class _SecondScreenState extends State<SecondScreen> {
       body: Column(
         children: [
           SizedBox(
-            height: 200,
+            height: 600,
             child: Expanded(
               child: ListView.builder(
                   itemCount: currentState.length,
                   itemBuilder: (context, index){
                     // A bad way to achieve the latest state, a better way is by state management like Providers
-                    return onVegetablesScreen?CustomCheckBox(objectName: currentState[index],):CustomCheckBox(objectName: currentState[index],);
+                    return CustomCheckBox(objectName: currentState[index],);
                   }),
             ),
           ),
          ElevatedButton(onPressed: (){
-           setState(() {
-             if(!onVegetablesScreen){
-               appBarTitle='Vegetables';
-               currentState=Constants.vegetablesMap.keys.toList();
-               onVegetablesScreen=true;
-             }
-             else{
-               Navigator.of(context).push(MaterialPageRoute(builder: (context)=>const FinalScreen()));
-             }
-           });
-          }, child: const Text('Next', style: TextStyle(color: Colors.black),))
+               Navigator.of(context).push(MaterialPageRoute(builder: (context)=>const ThirdScreen()));
+               },
+             child: const Text('Next', style: TextStyle(color: Colors.black),))
         ],
       )
     );
